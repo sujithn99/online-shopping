0054255f96ed36fc943dfea57dc86a4eed562e8b
sujithn99 <sujithkumar.vtalent@gmail.com>
2019-07-18 14:10:59 +0530

    added frontend css, js, images

---
+++ onlineshopping/src/main/java/net/nsk/onlineshopping/controller/PageController.java
@@ -0,0 +1,43 @@
+package net.nsk.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value= {"/", "/home", "/index"})
+	public ModelAndView index() {
+		System.out.println("request histting");
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("greeting","welcome to spring mvc");
+		return mv;
+	}
+	
+	
+	
+	/*@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting) {
+		if(greeting == null) {
+			greeting= "hello sujith";
+		}
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting" , greeting);
+		return mv;
+		
+	}*/
+	/*
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting") String greeting) {
+		if(greeting == null) {
+			greeting= "hello sujith";
+		}
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting" , greeting);
+		return mv;
+		
+	}*/
+}
